a948a8f77dd2cca3ca83d6e06ed8730b030be816
oscarn7992 <oscarn7992@G004PC07>
2023-01-05 14:21:38 +0000

    Add project files.

---
+++ stock-control/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("stock-control")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("St. John Rigby College")>
+<Assembly: AssemblyProduct("stock-control")>
+<Assembly: AssemblyCopyright("Copyright © St. John Rigby College 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9ff0d0b3-3c27-4e19-b460-916f42d7b68a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
