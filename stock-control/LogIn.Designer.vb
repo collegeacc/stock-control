0d954d8c50ee5becde9010a8601c7ae2a205338b
zonexstricker <oscarcraftstyle@gmail.com>
2023-01-19 18:42:04 +0000

    changed name of log in

--- stock-control/LogIn.Designer.vb
+++ stock-control/LogIn.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class MainMenu
+Partial Class LogIn
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
